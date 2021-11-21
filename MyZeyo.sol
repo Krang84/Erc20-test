d2c2a10a08a18a13805a3193877127baf3b3dcec
Krang84 <patelassad@gmail.com>
2021-11-21 14:00:25 +0100

    Update MyZeyo.sol

--- MyZeyo.sol
+++ MyZeyo.sol
@@ -1,11 +1,24 @@
 pragma solidity ^0.8.5;
 
 
+address public admin ;
+
 // Contrat erc-20 importé du github d'openZepplin
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/9b3710465583284b8c4c5d2245749246bb2e0094/contracts/token/ERC20/ERC20.sol' ;
 
 
 contract MyZeyo is ERC20 {
 
+   constructor(uint256 initialSupply) ERC20('CID93', 'CID') {
+        _mint(msg.sender, initialSupply) ;
+        admin = msg.sender
+   }
+   
+   
+   function mint(address to, uint amount) external {
+      require (msg.sender == admin, 'amdin only');
+      _mint(to, amount);
+   
+   }
 
 }
