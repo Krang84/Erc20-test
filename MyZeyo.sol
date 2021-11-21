84179aaa36bdd69f181316fb9d427202bff5015f
Krang84 <patelassad@gmail.com>
2021-11-21 13:42:24 +0100

    Create MyZeyo.sol

---
+++ MyZeyo.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.5;
+
+
+// Contrat erc-20 importé du github d'openZepplin
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/9b3710465583284b8c4c5d2245749246bb2e0094/contracts/token/ERC20/ERC20.sol' ;
+
+
+contract MyZeyo is ERC20 {
+
+
+}
